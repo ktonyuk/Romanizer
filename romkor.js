377122002e6a3adcdfffca7763f336954e5c83a5
Kateryna <ktonyuk@gmail.com>
2021-01-08 20:49:52 -0800

    Update romkor.js

--- romkor.js
+++ romkor.js
@@ -8304,14 +8304,14 @@ if (find !== null) {
    		syl = syl.replace(find[i], find[i].replace(/ui/g, "ye"));
 	}
 }
-//
-//var find = syl.match(/[^aeiouygbdnwmkshp]anha[^aeiouygbdnwmkshp]/g);
-//if (find !== null) {
-//	console.log(find);
-//	for (i = 0; i < find.length; i++) {
-//   		syl = syl.replace(find[i], find[i].replace(/anha/g, "ana"));
-//	}
-//}
+//않아
+var find = syl.match(/[^aeiouygbdnwmkshp]anha[^aeiouygbdnwmkshp]/g);
+if (find !== null) {
+	console.log(find);
+	for (i = 0; i < find.length; i++) {
+   		syl = syl.replace(find[i], find[i].replace(/anha/g, "ana"));
+	}
+}
 	
 //individual words
 
@@ -8441,7 +8441,7 @@ syl = syl.replace(/anhjanha/g, "anjana"); //않잖아
 syl = syl.replace(/anhgiro/g, "angiro"); //않기로
 syl = syl.replace(/anheun/g, "aneun"); //않는
 syl = syl.replace(/anhneun/g, "anneun"); //않는 
-syl = syl.replace(/않아/g, "ana"); //않아
+//syl = syl.replace(/anha/g, "ana"); //않아
 syl = syl.replace(/anheul/g, "aneul"); //않을 
 syl = syl.replace(/anhgo/g, "ango"); //않고
 syl = syl.replace(/janha/g, "jana"); //잖아 
